c5f56d88dc44c2043f28a7f25fbf2a4040b2db93
hafid selbi <selbihafid@gmail.com>
2025-08-08 00:45:13 +0100

    fix some troubleshooting env

--- node.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const credentials = fs.readFileSync('./credentials/service-account.json', 'utf8');
-const encodedCredentials = Buffer.from(credentials).toString('base64');
-console.log(encodedCredentials);
